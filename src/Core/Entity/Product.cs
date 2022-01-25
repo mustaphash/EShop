acae152e5c5f7f56d422a5ecf0f3a9af3cc9f5cb
Mustafa Shakir <79004067+mustaphash@users.noreply.github.com>
2022-01-25 23:55:47 +0200

    Add files via upload

---
+++ src/Core/Entity/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ src/Core/Entity/Shop.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entity
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime OpeningDate { get; set; }
+    }
+}
