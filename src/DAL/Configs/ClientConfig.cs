a922805d9ec29c62a13d6f96529a4537b620423c
Mustafa Shakir <mshakir9918@gmail.com>
2022-01-26 00:16:14 +0200

    Add all Configs

---
+++ src/DAL/Configs/ClientConfig.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class ClientConfig : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.FirstName).HasMaxLength(250).IsRequired();
+            builder.Property(c => c.LastName).HasMaxLength(250).IsRequired();
+            builder.Property(c => c.Adress).IsRequired();
+            builder.Property(c => c.Gender).IsRequired();
+            builder.Property(c => c.Age).IsRequired();
+        }
+    }
+}
---
+++ src/DAL/Configs/ProductConfig.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name).HasMaxLength(250).IsRequired();
+            builder.Property(p => p.Size).IsRequired();
+            builder.Property(p => p.Price).IsRequired();
+            builder.Property(p => p.Color).IsRequired();
+            builder.Property(p => p.ManufactureDate).IsRequired();
+            builder.Property(p => p.Count).IsRequired();
+        }
+    }
+}
