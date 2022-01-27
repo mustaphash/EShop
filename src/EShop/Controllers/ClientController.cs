bac17898d7fef82f2a59a3f01d37b5e6e69e455a
Mustafa Shakir <mshakir9918@gmail.com>
2022-01-28 00:50:18 +0200

    Add ClientController

---
+++ src/EShop/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Queries;
+using DAL.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Controllers
+{
+    [ApiController]
+    [Route("clients")]
+    public class ClientController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllClientsQuery, IList<Client>> _getAllClientsQuery;
+        public ClientController(
+            IQueryHandler<GetAllClientsQuery, IList<Client>> getAllClientsQuery)
+        {
+            _getAllClientsQuery = getAllClientsQuery;
+        }
+
+        [HttpGet(Name = "GetAllClients")]
+        public async Task<IActionResult> GetAllClients()
+        {
+            IList<Client> clients = await _getAllClientsQuery.HandleAsync(new GetAllClientsQuery());
+
+            return Ok(clients);
+        }
+    }
+}
