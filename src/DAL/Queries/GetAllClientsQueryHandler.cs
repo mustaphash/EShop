5e3f0f14afe59ea353a47bb56a946aa5d47287ba
Mustafa Shakir <mshakir9918@gmail.com>
2022-01-28 00:12:09 +0200

    Add Query and QueryHandler for Clients

---
+++ src/DAL/Queries/GetAllClientsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entity;
+using Core.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Queries
+{
+    public class GetAllClientsQueryHandler : IQueryHandler<GetAllClientsQuery, IList<Client>>
+    {
+        private readonly ShopContext _shopContext;
+        public GetAllClientsQueryHandler(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+        public async Task<IList<Client>> HandleAsync(GetAllClientsQuery query, CancellationToken cancellationToken = default)
+        {
+            List<Client> clients = await _shopContext.Clients.ToListAsync(cancellationToken);
+
+            return clients;
+        }
+    }
+}
